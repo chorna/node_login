c7fff134da32d8b13cf6f14770fba43b4e646233
Christian Horna <christian1020@gmail.com>
2023-10-31 00:27:04 -0500

    crypt password

--- components/user/controller.js
+++ components/user/controller.js
@@ -1,4 +1,5 @@
 const store = require('./store');
+const bcrypt = require('bcrypt')
 
 addUser = (username, password, first_name, last_name, email) => {
     return new Promise((resolve, reject) => {
@@ -6,16 +7,26 @@ addUser = (username, password, first_name, last_name, email) => {
             return reject('Datos incorrectos');
         }
 
-        const fulluser = {
-            username: username,
-            password: password,
-            first_name: first_name,
-            last_name: last_name,
-            email: email,
-        };
+        const saltRounds = 5;
 
-        store.add(fulluser);
-        resolve(fulluser)
+        const submitPassword = password
+
+        bcrypt.hash(submitPassword, saltRounds, (err, hash) => {
+            if (err) {
+                console.error('Error al encriptar la contraseña:', err);
+            } else {
+                const fulluser = {
+                    username: username,
+                    password: hash,
+                    first_name: first_name,
+                    last_name: last_name,
+                    email: email,
+                };
+
+                store.add(fulluser);
+                resolve(fulluser)
+            }
+        });
 
     });
 };
